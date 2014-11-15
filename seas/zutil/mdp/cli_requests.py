5334042669caf9ac8bb682be3b82b875e1193aad
Rick Copeland <rick@synapp.io>
2014-11-15 18:23:55 -0500

    Simplify the requests adapter for the majordomo protocol
    Signed-off-by: Rick Copeland <rick@synapp.io>

--- seas/zutil/mdp/cli_requests.py
+++ seas/zutil/mdp/cli_requests.py
@@ -12,7 +12,7 @@ class MajorDomoAdapter(BaseAdapter):
         super(MajorDomoAdapter, self).__init__()
         self.timeout = timeout
         self.retries = retries
-        self.clients = {}
+        self._client = None
 
     def send(self, req, **kwargs):
         parsed = urlparse(req.url)
@@ -26,47 +26,33 @@ class MajorDomoAdapter(BaseAdapter):
         resp.status_code = 200
         resp._content = ''.join(raw)
         return resp
+
+    def get_client(self, netloc):
+        if self._client is None:
+            self._client = self._create_client(netloc)
+        return self._client
 
     def close(self):
         import ipdb; ipdb.set_trace();
         raise NotImplementedError
 
-    def get_client(self, netloc):
-        client = self.clients.get(netloc)
-        if client is None:
-            client = self.clients[netloc] = MajorDomoClient(
-                'tcp://' + netloc,
-                timeout=self.timeout,
-                retries=self.retries)
-        return client
+    def _create_client(self, netloc):
+        return MajorDomoClient(
+            'tcp://' + netloc,
+            timeout=self.timeout,
+            retries=self.retries)
 
 
 class SecureMajorDomoAdapter(MajorDomoAdapter):
 
-    def __init__(self, client_key, server_keys, *args, **kwargs):
+    def __init__(self, client_key, server_key, *args, **kwargs):
         super(SecureMajorDomoAdapter, self).__init__(*args, **kwargs)
         self.client_key = client_key
-        self.server_keys = server_keys
+        self.server_key = server_key
 
-    def send(self, req, **kwargs):
-        parsed = urlparse(req.url)
-        cli = self.get_client(parsed.netloc)
-        service = parsed.path[1:].encode('utf-8')
-        if req.body:
-            raw = cli.send(service, req.body.encode('utf-8'))
-        else:
-            raw = cli.send(service)
-        resp = Response()
-        resp.status_code = 200
-        resp._content = ''.join(raw)
-        return resp
-
-    def get_client(self, netloc):
-        client = self.clients.get(netloc)
-        if client is None:
-            client = self.clients[netloc] = SecureMajorDomoClient(
-                self.server_keys[netloc], self.client_key,
-                'tcp://' + netloc,
-                timeout=self.timeout,
-                retries=self.retries)
-        return client
+    def _create_client(self, netloc):
+        return SecureMajorDomoClient(
+            self.server_key, self.client_key,
+            'tcp://' + netloc,
+            timeout=self.timeout,
+            retries=self.retries)
