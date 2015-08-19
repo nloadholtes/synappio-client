b8062141eb3ae849bcc6467f7430d4c017830122
Rick Copeland <rick@synapp.io>
2015-08-19 17:27:01 -0400

    Add JWT authorization policy
    Signed-off-by: Rick Copeland <rick@synapp.io>

---
+++ seas/jwt_auth.py
@@ -0,0 +1,43 @@
+import logging
+
+import jwt
+
+from pyramid.authentication import CallbackAuthenticationPolicy
+from pyramid.security import Authenticated, Everyone
+
+
+log = logging.getLogger(__name__)
+
+
+class JWTAuthenticationPolicy(CallbackAuthenticationPolicy):
+
+    def __init__(self, secret):
+        self.secret = secret
+
+    def unauthenticated_userid(self, request):
+        header = request.headers.get('Authorization', None)
+        request.claims = dict(sub='', grp='')
+        try:
+            bearer, data = header.split(' ')
+            if bearer.lower() != 'bearer':
+                return
+            claims = jwt.decode(data, self.secret)
+            request.claims = claims
+            return claims['sub']
+        except Exception as err:
+            log.exception('Error getting jwt from %s: %s', header, err)
+            return None
+
+    def callback(self, userid, request):
+        groups = request.claims['grp'].split(' ')
+        groups = ['group:' + g for g in groups]
+        result = ['account:' + userid] + groups
+        result.append(Authenticated)
+        result.append(Everyone)
+        return result
+
+    def remember(self, request, principal, **kw):  # pragma no cover
+        return []
+
+    def forget(self, request):  # pragma no cover
+        return []
